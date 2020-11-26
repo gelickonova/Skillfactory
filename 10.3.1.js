b94b981fcb118bbef14f25ceac1df509c70075e6
gelickonova <lera.gelickonova@gmail.com>
2020-11-26 21:56:26 +0300

    10.3.1 update

--- 10.3.1.js
+++ 10.3.1.js
@@ -4,8 +4,8 @@ const x = prompt("Введите число:");
 
 const numberX = +x;
 
-y = typeof numberX;
-if (y == "number") {
+const y = typeof numberX;
+if (y === "number") {
   if (isNaN (numberX)) alert("Упс, кажется вы ошиблись");
   else 
     if (x % 2 === 0) alert(`Число ${numberX} чётное`);
