1676d374bef4d512135c442ae515d9eeabfe29d0
gelickonova <lera.gelickonova@gmail.com>
2020-11-26 21:45:56 +0300

    10.3.1

---
+++ 10.3.1.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const x = prompt("Введите число:");
+
+const numberX = +x;
+
+y = typeof numberX;
+if (y == "number") {
+  if (isNaN (numberX)) alert("Упс, кажется вы ошиблись");
+  else 
+    if (x % 2 === 0) alert(`Число ${numberX} чётное`);
+    else alert(`Число ${numberX} нечётное`);
+}
+else alert("Упс, кажется вы ошиблись");
