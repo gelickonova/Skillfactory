e1a7371b714aec76b6dbbf555de91d35ee3fc9e3
gelickonova <lera.gelickonova@gmail.com>
2020-12-02 18:19:06 +0300

    add 10.3.2

---
+++ 10.3.2.js
@@ -0,0 +1,21 @@
+function func(x){
+    let y = typeof x;
+    switch(y){
+        case 'number':
+            console.log (`${x} - число`);
+            break;
+        case 'boolean':
+            console.log (`${x} - логический тип`);
+            break;
+        case 'string':
+            console.log (`${x} - строка`);
+            break;
+        default:
+            console.log (`Тип ${x} не определен`);
+    }
+}
+// на этих примерах вроде работает как надо:
+// func(123);
+// func('привет');
+// func(false);
+// func(undefined);
